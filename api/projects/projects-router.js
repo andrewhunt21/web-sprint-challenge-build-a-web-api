5ef4e2d34e4def61ae462413dcf9cb19656dc61a
andrewhunt21 <andrewhunt21@icloud.com>
2022-01-14 09:39:05 -0800

    get endpoint for projects works

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -6,6 +6,19 @@ const Projects = require('./projects-model')
 
 const router = express.Router()
 
+router.get('/', (req, res, next) => {
+    Projects.get()
+        .then(projects => {
+            res.json(projects)
+        })
+        .catch(next)
+})
 
+router.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+        message: err.message,
+        stack: err.stack
+    })
+})
 
 module.exports = router
